e11561e8d568dabe9493d423effd6c35653bfc3c
OlivierBelaud <obelaud@fluks.com>
2019-02-11 01:53:32 +0100

    add font awesome

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const path = require('path')
 
 module.exports = function nuxtBulma(options) {
   // Add CSS
-  this.options.css.push('~/assets/css/main.scss')
+  this.options.css.push({src: '~/assets/css/main.scss', lang: 'scss'})
+  this.options.css.push({src: '@fortawesome/fontawesome-free/css/all.css'})
 }
 
 module.exports.meta = require('./package.json')
